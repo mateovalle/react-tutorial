c8746a610106bdb0a43cdecc3969da3ecc097c6e
mateovalle <mateo.valle@ing.austral.edu.ar>
2022-02-08 10:41:38 -0300

    functional components

---
+++ tic-tac-toe/src/components/Game.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+import Squares from './Square';
+import '../index.css';
+
+function Game(){
+    const [history, setHistory] = useState([{squares: Array(9).fill(null),}]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXIsNext] = useState(true);
+    const [status, setStatus] = useState('');
+
+    
+
+    const current = history[stepNumber];
+    
+    const moves = history.map((step, move) =>{
+        const desc = move? 'Go to move #' + move : 'Go to game start';
+        return(
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    useEffect(() => {
+        const winner = calculateWinner(current.squares);
+        if (winner) {
+            setStatus('Winner: ' + winner);
+        } else {
+            setStatus('Next player: ' + (xIsNext ? 'X' : 'O'));
+        }
+    }, [history]);
+
+    const handleClick = (i) => {
+        const historyAux = history.slice(0, stepNumber+1);
+        const current = historyAux[historyAux.length -1];
+        const squares = current.squares.slice();
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory(historyAux.concat([{
+            squares: squares,
+        }]));
+        setStepNumber(historyAux.length);
+        setXIsNext(!xIsNext);
+    }
+
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext((step%2) === 0);
+    }
+
+    return (
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares = {current.squares}
+              onClick={(i) => handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] ===squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+export default Game;
